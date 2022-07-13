9f4d742f77fd7b62745e1c64089e4a8cf2286e1a
iTetreX <85939949+iTetreX@users.noreply.github.com>
2022-07-13 21:56:58 +0300

    Update SoftShutdown.lua

--- SoftShutdown.lua
+++ SoftShutdown.lua
@@ -1,9 +1,1 @@
-local Requirements = {
-    ["Shutdown"] = {
-        ["WaitTime"] = 30,
-        ["UpdateText"] = "New Update!",
-        ["UpdateDescription"] = "New update has released! Hold on couple second."
-    }
-}
-
-return Requirements
+print("sa")
